69b9992364957d107ea47eb6646ca3b6c6cad077
subbiah <subbiah.sam@gmail.com>
2020-05-08 16:04:54 -0500

    added oAuth Integration to sample app

---
+++ src/main/java/com/example/gmailoauthintegration/GithubOauthIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmailoauthintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubOauthIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubOauthIntegrationApplication.class, args);
+	}
+
+}
